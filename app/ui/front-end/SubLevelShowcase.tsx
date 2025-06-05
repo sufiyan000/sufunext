1adeb4a1821abe07a785ea061c66461ec01c8471
sufiyan_akhtar <sufiyanakhtar69@gmail.com>
2025-06-06 02:13:15 +0530

    levelImageshow

--- app/ui/front-end/SubLevelShowcase.tsx
+++ app/ui/front-end/SubLevelShowcase.tsx
@@ -1,24 +1,14 @@
 "use client";
-import {
-  CubeIcon, BookOpenIcon, HeartIcon, HomeIcon, ShoppingBagIcon, ComputerDesktopIcon
-} from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface SubLevel {
   _id: string;
   name: string;
   slug: string;
+  image: string;
 }
-
-const iconMap: Record<string, React.ElementType> = {
-  "Lipstick": HeartIcon,
-  "Foundation": CubeIcon,
-  "Notebooks": BookOpenIcon,
-  "Wall Art": HomeIcon,
-  "Dresses": ShoppingBagIcon,
-  "Mobile Chargers": ComputerDesktopIcon,
-};
 
 const SubLevelShowcase = ({ sublevels }: { sublevels: SubLevel[] }) => {
   return (
@@ -28,14 +18,21 @@ const SubLevelShowcase = ({ sublevels }: { sublevels: SubLevel[] }) => {
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
         {sublevels.map((level) => {
-          const Icon = iconMap[level.name] || CubeIcon;
+          
           return (
             <Link
               href={`/products/levels/${level.slug}`}
               key={level._id}
               className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center justify-center transition-transform transform hover:scale-105"
             >
-              <Icon className="h-14 w-14 text-[#07f0f0] mb-4" />
+              {level.image?.trim() && (
+                  <Image
+                    src={level.image}
+                    alt={level.name}
+                    width={100}
+                    height={100}
+                  />
+              )}
               <h3 className={`${lusitana.className} text-xl font-semibold text-gray-700 text-center`}>
                 {level.name}
               </h3>
