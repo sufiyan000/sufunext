4553f60c50abbcc5097fb8e97ab89da9541d9dfd
sufiyan_akhtar <sufiyanakhtar69@gmail.com>
2025-06-07 10:12:30 +0530

    solvecatpic

--- app/ui/front-end/category-showcase.tsx
+++ app/ui/front-end/category-showcase.tsx
@@ -1,14 +1,4 @@
 "use client";
-import {
-  ShoppingCartIcon,
-  HomeIcon,
-  BookOpenIcon,
-  ShoppingBagIcon,
-  PlusIcon,
-  HeartIcon,
-  ComputerDesktopIcon,
-  CubeIcon
-} from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
 
